fe2b315cd9ee82c0e0503daca2367bda277c2662
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-04-11 00:26:26 +0300

    Final version of this project

---
+++ public/spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet" tilewidth="16" tileheight="16" tilecount="1209" columns="39">
+ <image source="spritesheet.png" width="624" height="496"/>
+</tileset>
---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JAVA/TypeScript these days.
+    I've made a couple of games in that language. I also like Lua and Typescript. Anyway regardless of the language, I just like programming. And I use Arch btw 😎.
+    Here is my <a href="https://github.com/AmigosKazz" target="_blank">Github</a>!`,
+    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube videos or watch a twich videos with ThePrimeagen.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a> (my fav 😁), <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@amigoscode" target="_blank">Amigoscode</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/AmigosKazz/ResumeCV/blob/master/ResumeEnglish.pdf" target="_blank">Check it out?</a>
+  Contact me at kaznarahandrinarivo@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  I probably only read one of them. I mostly learn from the internet. But I like to have them around because they make me look smart 😁.`,
+    exit: `If you want to exit AmigosKazz's portfolio, just close the tab.👋`,
+};
