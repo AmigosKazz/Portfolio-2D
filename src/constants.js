f4d86533be3c52f1d64c20fe32f7d19eee2db84d
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-05-07 21:23:56 +0300

    Update dialogueData in constants.js and fix file path in workspace.xml

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,7 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JAVA/TypeScript these days.
-    I've made a couple of games in Lua and JS I also like Rust. Anyway regardless of the language, I just like programming. And I use Arch btw 😎.
+    pc: `This is my PC. I mainly work in JAVA/TypeScript these days, but I also enjoy developing video games and I like using Lua and JS. I also like Rust. Anyway regardless of the language, I just like programming. And I use Arch btw 😎.
     Here is my <a href="https://github.com/AmigosKazz" target="_blank">Github</a>!`,
     "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube videos or watch a twich videos with ThePrimeagen.`,
@@ -13,8 +12,7 @@ export const dialogueData = {
     resume: `This is my desk and on it is my resume. <a href="https://github.com/AmigosKazz/ResumeCV/blob/master/ResumeEnglish.pdf" target="_blank">Check it out?</a>
   Contact me at kaznarahandrinarivo@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
     library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. I mostly learn from the internet. But I like to have them around because they make me look smart 😁.`,
     exit: `If you want to exit AmigosKazz's portfolio, just close the tab.👋`,
